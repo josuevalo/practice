9f999054730afc1ce47d5b8327b45275d50c9213
Josue Arevalo <arevalo.josued@gmail.com>
2023-01-03 22:07:43 -0800

    Add in instructions on first leetcode exercise for ref

--- search-insert-position.js
+++ search-insert-position.js
@@ -1,6 +1,10 @@
 // Kata from Leetcode // 
 
 
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order. //
+
+//  You must write an algorithm with O(log n) runtime complexity. //
+
 var searchInsert = function(nums, target) {
   if ( nums.indexOf(target) > 0) {
       return nums.indexOf(target)
@@ -19,4 +23,4 @@ var searchInsert = function(nums, target) {
 
 console.log(searchInsert([1,3,5,6], 5));
 
-// The above console log should return 2 //
+// The above console log should return 2 
