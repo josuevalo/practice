d5a15a882f108e3b9026fdd68b39400a2e2a0877
Josue Arevalo <arevalo.josued@gmail.com>
2023-01-08 21:03:40 -0800

    add two int instructions

--- add-two-integers.js
+++ add-two-integers.js
@@ -1,5 +1,7 @@
 // Kata from Leetcode // 
 
+// Given two integers num1 and num2, return the sum of the two integers.
+
 var sum = function(num1, num2) {
   return num1 + num2
 };
---
+++ concat-array.js
@@ -0,0 +1,1 @@
+// Kata from Leetcode // 
