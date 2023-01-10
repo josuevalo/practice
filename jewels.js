e822a2d387810dadebbec8e64cb94606d9e4f340
Josue Arevalo <arevalo.josued@gmail.com>
2023-01-09 21:53:30 -0800

    jewels kata

---
+++ jewels.js
@@ -0,0 +1,19 @@
+// Kata from Leetcode // 
+
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+var numJewelsInStones = function(jewels, stones) {
+  let runningCount = 0;
+for (let i = 0; i < jewels.length; i++) {
+  for (let j = 0; j < stones.length; j++){
+    if (jewels[i] === stones[j]){
+      runningCount++
+    }
+  }
+}
+  return runningCount
+};
+
+console.log(numJewelsInStones("aA", "aAAbbbb"));
