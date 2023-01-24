e1923222e0e8c118b8a11651b9dc8855183ac3c3
Josue Arevalo <arevalo.josued@gmail.com>
2023-01-24 13:25:26 -0800

    changes

--- find.js
+++ find.js
@@ -14,5 +14,5 @@
 
 function findNeedle(haystack) {
   // your code here
-  return `found the needle at position ${haystack.indexOf("needle")}`
+  return `found the needle at position ${haystack.indexOf("needle")}`;
 }
