6e5d308137e2edd44b7ff3858af5a0431b2c1e5e
Josue Arevalo <arevalo.josued@gmail.com>
2023-01-27 21:52:25 -0800

    only one kata

---
+++ only-one.js
@@ -0,0 +1,17 @@
+// Kata from Codewars //
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+  // your code here
+  for (i = 0; i < a.length; i++) {
+    if (a[i] === x) {
+      return true;
+    }
+  }
+  return false;
+}
