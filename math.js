ef7d5049c14a1cb6759b3729573156237fc5a7f9
Josue Arevalo <arevalo.josued@gmail.com>
2023-02-06 22:50:26 -0800

    math kata

---
+++ math.js
@@ -0,0 +1,13 @@
+// Kata from CodeWars //
+
+function arithmetic(a, b, operator){
+  //your code here!
+  if (operator === "add"){
+    return a + b
+  } else if (operator === "subtract") {
+    return a - b
+  } else if (operator === "multiply") {
+    return a * b
+  } else if (operator === "divide") {
+    return a / b
+  }
