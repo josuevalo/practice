ab7957f87a0596e53d796929cff0fe56fe6802e6
Josue Arevalo <arevalo.josued@gmail.com>
2023-02-11 23:45:44 -0800

    refactor

--- grasshopper.js
+++ grasshopper.js
@@ -1,5 +1,7 @@
-// Kata from Codewars //
-function move(position, roll) {
-  // return the new position
-  return position + roll + roll;
-}
+// // Kata from Codewars //
+// function move(position, roll) {
+//   // return the new position
+//   return position + roll + roll;
+// }
+
+const move = (position, roll) => position + roll * 2
