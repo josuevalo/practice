4f1715db42fb2f2f9d996171f8ff24497701c635
Josue Arevalo <arevalo.josued@gmail.com>
2023-03-16 22:47:12 -0700

    refactor

---
+++ refactor.js
@@ -0,0 +1,15 @@
+const loopyLighthouse = function (range, multiples, words) {
+  for (let x = range[0]; x <= range[1]; x++) {
+    let multipleZero = x % multiples[0] === 0;
+    let multipleOne = x % multiples[1] === 0;
+    if (multipleZero && multipleOne) {
+      console.log(words[0] + words[1]);
+    } else if (multipleZero) {
+      console.log(words[0]);
+    } else if (multipleOne) {
+      console.log(words[1]);
+    } else if (x < range[1]) {
+      console.log(x);
+    }
+  }
+};
