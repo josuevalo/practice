a3514ad32a495d195460e8709df373214a6a4ea7
Josue Arevalo <arevalo.josued@gmail.com>
2023-03-19 22:59:13 -0700

    hoop test kata

---
+++ hooptest.js
@@ -0,0 +1,9 @@
+// Kata from Codewars //
+function hoopCount(n) {
+  //your code goes here
+  if (n >= 10) {
+    return "Great, now move on to tricks";
+  } else {
+    return "Keep at it until you get it";
+  }
+}
