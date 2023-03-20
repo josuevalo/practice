df18146ae769ce57f90703fcb5444a7fc60a68e7
Josue Arevalo <arevalo.josued@gmail.com>
2023-03-19 23:00:08 -0700

    refactor hoop test kata

--- hooptest.js
+++ hooptest.js
@@ -1,9 +1,15 @@
 // Kata from Codewars //
+// function hoopCount(n) {
+//   //your code goes here
+//   if (n >= 10) {
+//     return "Great, now move on to tricks";
+//   } else {
+//     return "Keep at it until you get it";
+//   }
+// }
+
 function hoopCount(n) {
-  //your code goes here
-  if (n >= 10) {
-    return "Great, now move on to tricks";
-  } else {
-    return "Keep at it until you get it";
-  }
+  return n < 10
+    ? "Keep at it until you get it"
+    : "Great, now move on to tricks";
 }
